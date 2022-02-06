845fd3377fb5ded6bbf8096113314c36da46f39c
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2022-02-06 09:55:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ function start(){
 navigator.mediaDevices.getUserMedia({audio:true});
 classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/3q3Q45YSt/model.json",modelReady);
 }
+
+function modelReady(){
+
+classifier.classify(gotResults);
+}
+
+function gotResults(error,results){
+
+if(error){
+
+console.error(error)
+
+}
+else{
+
+
+    
+}
+
+}
