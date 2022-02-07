82630cf670e4e4ef32a8f8289f9c1c29c19b3889
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2022-02-07 14:31:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,6 +18,33 @@ console.error(error)
 }
 else{
 
+console.log(results);
+document.getElementById("label_name").innerHTML = results[0].label;
+document.getElementById("label_accuracy").innerHTML = (results[0].confidence * 100).toFixed(2);
+
+var i1 = document.getElementById("rep_image");
+
+if(results[0].label == "Barking"){
+
+i1.src= "dog.gif"
+
+}
+else if(results[0].label == "Neighing"){
+
+i1.src = "horse.png"
+
+}
+else if(results[0].label == "Mooing"){
+
+    i1.src = "cow.png"
+    
+}
+else if(results[0].label == "Roaring"){
+
+    i1.src = "lion.png"
+    
+    }
+
 
     
 }
